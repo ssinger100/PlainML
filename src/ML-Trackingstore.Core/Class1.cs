bba8db71febe6f2a9e75dc9bf3ad7f211591d113
Stefan Singer <st.singer@outlook.com>
2022-06-15 14:31:17 +0200

    Add some projects

---
+++ src/ML-Trackingstore.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ML_Trackingstore;
+public class Class1
+{
+
+}
---
+++ tests/IntegrationTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
