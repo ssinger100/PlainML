90707e29ace390381c325bfd092e58d470b34d8b
Stefan Singer <st.singer@outlook.com>
2022-12-17 12:48:43 +0100

    b

--- src/ML-Trackingstore.Core/Entities/Deployment.cs
+++ src/ML-Trackingstore.Core/Entities/Deployment.cs
@@ -2,7 +2,14 @@
 
 public class Deployment
 {
+    public int Id { get; set; }
+
+    public int ExperimentId { get; set; }
     public Experiment Experiment { get; set; }
+
+    public int DeploymenttargetId { get; set; }
     public Deploymenttarget Deploymenttarget { get; set; }
+
+    public int RunId { get; set; }
     public Run Run { get; set; }
 }
--- src/ML-Trackingstore/Configurations/DeploymentConfiguration.cs
+++ src/ML-Trackingstore/Configurations/DeploymentConfiguration.cs
@@ -8,6 +8,6 @@ internal class DeploymentConfiguration : IEntityTypeConfiguration<Deployment>
 {
     public void Configure(EntityTypeBuilder<Deployment> builder)
     {
-        builder.HasIndex(x => new { ExperimentId = x.Experiment.Id, DeploymenttargerId = x.Deploymenttarget.Id,RunId = x.Run.Id });
+        builder.HasIndex(x => new { ExperimentId = x.ExperimentId, DeploymenttargerId = x.DeploymenttargetId,RunId = x.RunId });
     }
 }
