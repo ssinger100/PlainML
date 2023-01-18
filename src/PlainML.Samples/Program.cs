f7dd1a003149ec9d59251791315334d3329e12df
Stefan Singer <ssinger100@outlook.com>
2023-01-18 13:24:33 +0100

    add example

--- src/PlainML.Samples/Program.cs
+++ src/PlainML.Samples/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using PlainML;
+using PlainML.Infrastructure;
+
 Console.WriteLine("Hello, World!");
+
+var _provider = new ServiceCollection()
+            .UseSQLLite()
+            .UseFilesystem()
+            .AddTransient<PlainMLService>()
+            .BuildServiceProvider();
+
+PlainMLService s = _provider.GetRequiredService<PlainMLService>();
+await s.Migrate();
+int rundId = await s.StartRun("TestExperiment");
+await Task.Delay(100);
+await s.EndRun(rundId, null, null, null);
